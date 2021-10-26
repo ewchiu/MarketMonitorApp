2727464afc03d096b0429f7b5f26ec4abd095ec6
Eric Chiu <ericwchiu@gmail.com>
2021-10-25 17:05:18 -0700

    added supporting classes for data retrieval

---
+++ InputProcessing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketMonitorApp
+{
+    class InputProcessing
+    {
+        public void RetrieveInput(string ticker)
+        {
+
+        }
+    }
+}
---
+++ StockInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketMonitorApp
+{
+    public class StockInfo
+    {
+        public string ticker { get; set; }
+        public int currentPrice { get; set; }
+        public int yearHigh { get; set; }
+        public int peRatio { get; set; }
+    }
+}
